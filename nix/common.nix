c221e2e32bc454f6ed4143557e55f13adf122e4d
Peter A. <ink.splatters@pm.me>
2024-02-14 18:46:17 +0100

    * tbd files for Apple libsandbox (as a workaround for missing dylib in   /usr/lib (it was moved do dyld_shared_cache) * pre-commit hooks

--- nix/common.nix
+++ nix/common.nix
@@ -1,25 +1,16 @@
-{ pkgs, lib, llvmPackages, system, ... }:
+{ pkgs, lib, system, ... }:
 let
 
-  replaceStdenv = pkg:
-    pkg.overrideAttrs (_: { inherit (llvmPackages) stdenv; });
-
-  inherit (pkgs.darwin.apple_sdk) frameworks impure-deps;
+  inherit (pkgs.darwin.apple_sdk) frameworks;
 
 in rec {
-
   name = "awrit";
 
   CFLAGS = "-O3"
     + lib.optionalString ("${system}" == "aarch64-darwin") " -mcpu=apple-m1";
   CXXFLAGS = "${CFLAGS}";
-  LDFLAGS="";
-  # LDFLAGS = "-L/usr/lib -fuse-ld=lld  ";
+  LDFLAGS = "-fuse-ld=lld";
 
-  nativeBuildInputs = with pkgs;
-      [ ccache cmake ninja gitMinimal cacert]
-    # lld ]
-    ++ [ (replaceStdenv pkgs.xcodebuild) ];
-
-  buildInputs = with frameworks;[ Cocoa AppKit ];
+  nativeBuildInputs = with pkgs; [ cmake ninja gitMinimal lld ];
+  buildInputs = with frameworks; [ Cocoa AppKit ];
 }
