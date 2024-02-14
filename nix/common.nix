53d4d42c7d3e645bd90d762983b3475fce7815e0
Peter A. <ink.splatters@pm.me>
2024-02-14 17:35:47 +0100

    progress

--- nix/common.nix
+++ nix/common.nix
@@ -4,6 +4,8 @@ let
   replaceStdenv = pkg:
     pkg.overrideAttrs (_: { inherit (llvmPackages) stdenv; });
 
+  inherit (pkgs.darwin.apple_sdk) frameworks impure-deps;
+
 in rec {
 
   name = "awrit";
@@ -11,11 +13,13 @@ in rec {
   CFLAGS = "-O3"
     + lib.optionalString ("${system}" == "aarch64-darwin") " -mcpu=apple-m1";
   CXXFLAGS = "${CFLAGS}";
-  LDFLAGS = "-fuse-ld=lld";
+  LDFLAGS="";
+  # LDFLAGS = "-L/usr/lib -fuse-ld=lld  ";
 
   nativeBuildInputs = with pkgs;
-    [ ccache cmake ninja gitMinimal cacert lld ]
+      [ ccache cmake ninja gitMinimal cacert]
+    # lld ]
     ++ [ (replaceStdenv pkgs.xcodebuild) ];
 
-  buildInputs = with pkgs.darwin.apple_sdk.frameworks; [ Cocoa AppKit ];
+  buildInputs = with frameworks;[ Cocoa AppKit ];
 }
